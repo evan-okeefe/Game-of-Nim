c605e0ec853b1ab6cd1ed40cc57470ffcd0ffe24
Evan O'Keefe <73354669+evan-okeefe@users.noreply.github.com>
2025-03-24 10:32:38 -0700

    Isaac, I already finished the project

--- Nim/src/Game.java
+++ Nim/src/Game.java
@@ -10,20 +10,12 @@ public class Game {
     Scanner scanner = new Scanner(System.in);
 
     public void play() {
-        String response = "";
-        while (!(response.equalsIgnoreCase("p") || response.equalsIgnoreCase("c"))){
-            System.out.println("Would you like to play against another [p]layer, or against a [c]omputer?");
-            response = scanner.nextLine();
-        }
+        String response;
+        System.out.println("Would you like to play against another [P]layer, or against a [c]omputer?");
+        response = scanner.nextLine();
 
-        if (response.equalsIgnoreCase("p")){
-            System.out.println("Enter Player 1's name:");
-            p1 = new Player(scanner.nextLine());
 
-            System.out.println("Enter Player 2's name:");
-            p2 = new Player(scanner.nextLine());
-        }
-        else if (response.equalsIgnoreCase("c")){
+        if (response.equalsIgnoreCase("c")){
             System.out.println("Enter Player's name:");
             p1 = new Player(scanner.nextLine());
 
@@ -36,22 +28,66 @@ public class Game {
             }
             else{
                 System.out.println("Enter Computer's name:");
-                p2 = new Computer("[Computer]" + scanner.nextLine());
+                p2 = new Computer("[Computer] " + scanner.nextLine());
             }
 
+        }
+        else{
+            System.out.println("Enter Player 1's name:");
+            p1 = new Player(scanner.nextLine());
+
+            System.out.println("Enter Player 2's name:");
+            p2 = new Player(scanner.nextLine());
         }
 
         int playerTurn = (int)(Math.random()*2) + 1;
 
         System.out.println("Player " + playerTurn + " goes first: " + p1.getName() + " vs " + p2.getName());
 
+        loop();
+
     }
 
     public void loop(){
+        while(Board.getNumTiles() > 0){
+            if (playerTurn == 1){
+                System.out.println(p1.getName() + "'s turn!");
+                Board.display();
+                p1.takeTurn();
+                playerTurn = 2;
+            }
+            else{
+                System.out.println(p2.getName() + "'s turn!");
+                Board.display();
+                p2.takeTurn();
+                playerTurn = 1;
+            }
+        }
+
+        if (Board.getNumTiles() == 0){
+            if (playerTurn == 1){
+                System.out.println(p2.getName() + " loses");
+                restart();
+            }
+            else{
+                System.out.println(p1.getName() + " loses");
+                restart();
+            }
+        }
 
     }
 
     public void restart(){
+        System.out.println("Would you like to play again?");
+        System.out.println("[Y]es / [n]o");
+        String choice = scanner.nextLine();
 
+        if (choice.equalsIgnoreCase("n")){
+            System.out.println("Goodbye!");
+        }
+        else{
+            Board.populate();
+            play();
+        }
     }
 }
--- Nim/src/Player.java
+++ Nim/src/Player.java
@@ -1,21 +1,50 @@
+import java.util.Scanner;
+
 public class Player {
 
     private String name;
     private int score;
+
+    private Scanner scanner = new Scanner(System.in);
 
     public Player(String _name) {
         name = _name;
     }
 
     public void takeTurn(){
+        if (Board.getNumTiles() == 1)
+            System.out.println("Choose a number of tiles to remove (up to " + 1 + "):");
+        else
+            System.out.println("Choose a number of tiles to remove (up to " + Board.getNumTiles()/2 + "):");
+        int num = scanner.nextInt();
 
+        nim(num);
     }
 
     public void nim(int num){
-        if (num > Board.getNumTiles()/2){
-            score += num;
-            Board.takeTiles(num);
+        if (Board.getNumTiles() == 1){
+            if (num == 1){
+                System.out.println("You take " + num + " tiles");
+                score += num;
+                Board.takeTiles(num);
+            }
+            else{
+                System.out.println("You cannot take that many tiles");
+                takeTurn();
+            }
         }
+        else{
+            if (num <= Board.getNumTiles()/2){
+                System.out.println("You take " + num + " tiles");
+                score += num;
+                Board.takeTiles(num);
+            }
+            else{
+                System.out.println("You cannot take that many tiles");
+                takeTurn();
+            }
+        }
+
     }
 
     public String getName() {
